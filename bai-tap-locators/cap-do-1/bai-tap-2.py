3c84bbc72d770f560bad9041249913e387576af2
DESKTOP-TIQRLVA\ACER <thangtcq@gmail.com>
2025-10-06 01:38:37 +0700

    20251006 - Create bai-tap-locators\cap-do-1\bai-tap-2.py 20251006 - Create bai-tap-locators\cap-do-2\bai-tap-3.py

---
+++ bai-tap-locators/cap-do-1/bai-tap-2.py
@@ -0,0 +1,41 @@
+#ViTriSo1
+"vi-tri-so-1" = "//img[@alt='Airbnb Logo']//parent::a"
+
+#ViTriSo2
+"vi-tri-so-2" = "//a[text()='Home']"
+
+#ViTriSo3
+"vi-tri-so-3" = "//a[text()='About']"
+
+#ViTriSo4
+"vi-tri-so-4" = "//div[@id='user-dropdown']/parent::div"
+
+#ViTriSo5
+"vi-tri-so-5" = "//p[text()='Địa điểm']/parent::div"
+
+#ViTriSo6
+"vi-tri-so-6" = "//p[text()='Địa điểm']/parent::div/following-sibling::div[2]"
+
+#ViTriSo7
+"vi-tri-so-7" = "//p[text()='Thêm khách']/parent::div"
+
+#ViTriSo8
+"vi-tri-so-8" = "//p[text()='Thêm khách']/following-sibling::div//div[text()='+']"
+
+#ViTriSo9
+"vi-tri-so-9" = "//p[text()='Thêm khách']/following-sibling::div//div[text()='-']"
+
+#ViTriSo10
+"vi-tri-so-10" = "//h2[text()='Hồ Chí Minh']//ancestor::a"
+
+#ViTriSo11
+"vi-tri-so-11" = "//h2[text()='Cần Thơ']"
+
+#ViTriSo12
+"vi-tri-so-12" = "//button[text()='Loại nơi ở']"
+
+#ViTriSo13
+"vi-tri-so-13" = "//button[text()='Giá']"
+
+#ViTriSo14
+"vi-tri-so-14" = "//h2[text()='Nha Trang']/following-sibling::p"
---
+++ bai-tap-locators/cap-do-2/bai-tap-3.py
@@ -0,0 +1,39 @@
+#ViTriSo1
+"vi-tri-so-1" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']//ancestor::div[contains(@class,'a-spacing-small')]/preceding-sibling::div[contains(@class,'image')]"
+
+#ViTriSo2
+"vi-tri-so-2" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']//ancestor::div[@data-cy='title-recipe']/following-sibling::div[@data-cy='price-recipe']//div[contains(@aria-hidden,'List')]"
+
+#ViTriSo3
+"vi-tri-so-3" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']//ancestor::div[@data-cy='title-recipe']/following-sibling::div[@data-cy='price-recipe']//span[@class='a-price']"
+
+#ViTriSo4
+"vi-tri-so-4" = "//input[@id='twotabsearchtextbox']"
+
+#ViTriSo5
+"vi-tri-so-5" = "//a[@id='nav-cart']"
+
+#ViTriSo6
+"vi-tri-so-6" = "//a[text()=\"Women's Footwear\"]"
+
+#ViTriSo7
+"vi-tri-so-7-cach-1" = "//span[contains(@class,'a-size-base') and text()=6]"
+"vi-tri-so-7-cach-2" = "//ul[@id='filter-p_n_pt_nav_size_men_shoe']//button[@value='6']" # Best Choice
+
+#ViTriSo8
+"vi-tri-so-8" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']"
+
+#ViTriSo9
+"vi-tri-so-9" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']//ancestor::div[@data-cy='title-recipe']/following-sibling::div[@data-cy='delivery-recipe']//span[contains(@aria-label,'Delivery')]"
+
+#ViTriSo10
+"vi-tri-so-10" = "//*[text()='Boys’ Iconic Tricot Jogger Track Pants']//ancestor::div[@data-cy='title-recipe']//span[contains(@class,'a-size-base-plus')]"
+
+#ViTriSo11
+"vi-tri-so-11" = "//h2[@id='loom-desktop-top-slot_us-slds-sp-2-t1-a2-heading']"
+
+#ViTriSo12
+"vi-tri-so-12" = "//span[text()='Gender']/parent::div/following-sibling::ul//span[text()='Men']/preceding-sibling::div/label"
+
+#ViTriSo13
+"vi-tri-so-13" = "//span[contains(text(),'of over') and contains(text(),'results for')]"
